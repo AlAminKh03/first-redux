96b35a2d6126098f0b6c9ee71965b03bc7db8a22
AlAminKh03 <alaminkhan6203@gmail.com>
2022-07-19 20:15:23 +0600

    state added

--- fetchapi.js
+++ fetchapi.js
@@ -4,3 +4,11 @@ const { createStore } = require('redux')
 const GET_TODOS_REQUEST = "GET_TODOS_REQUEST"
 const GET_TODOS_SUCCESS = "GET_TODOS_SUCCESS"
 const GET_TODOS_FAILED = "GET_TODOS_FAILED"
+
+// state 
+
+const initialTodosState = {
+    todos: [],
+    isLoading: false,
+    err: null
+}
