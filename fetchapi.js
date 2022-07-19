8bfae3a8e634150353ca1848ef1322d81f9b0104
AlAminKh03 <alaminkhan6203@gmail.com>
2022-07-19 20:09:58 +0600

    fetch api's const added

---
+++ fetchapi.js
@@ -0,0 +1,6 @@
+const { createStore } = require('redux')
+
+// reuiring const 
+const GET_TODOS_REQUEST = "GET_TODOS_REQUEST"
+const GET_TODOS_SUCCESS = "GET_TODOS_SUCCESS"
+const GET_TODOS_FAILED = "GET_TODOS_FAILED"
